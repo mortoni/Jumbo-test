cf347fd8a67ee1748a2ecd3c239b3fba0393e0d9
Alan Mortoni <alan_mortoni@flightcentre.com>
2020-05-21 13:32:37 +1000

    fixed: setup theme and router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
-import { Box } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { createMuiTheme } from '@material-ui/core'
+import { Router } from '@reach/router'
+import theme from './theme'
+import { ROUTES } from './routes'
+
+import HomePage from './pages/Home'
+import DetailsPage from './pages/Details'
 
 function App() {
-  return <Box> oi</Box>
+  return (
+    <ThemeProvider theme={createMuiTheme(theme)}>
+      <CssBaseline />
+      <Router>
+        <HomePage path={ROUTES.HOME} />
+        <DetailsPage path={ROUTES.DETAILS} />
+      </Router>
+    </ThemeProvider>
+  )
 }
 
 export default App
